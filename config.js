acbdce39e9b7f57d8d655647f2ef85bc2eeb977c
SAANIIKHAN <151637481+SAANIIKHAN@users.noreply.github.com>
2023-11-21 17:44:23 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923329961286', 'SAANII BOT', true],
+  ['923179567658', 'KHAN', true], 
   [''] 
 ] //Number of owners
 
